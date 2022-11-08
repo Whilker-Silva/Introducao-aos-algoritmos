0d74d8bb3e3e2f036711fed835d02ce256718cfe
Whilker Henrique dos Santos Silva <77589533+Whilker-Silva@users.noreply.github.com>
2022-11-08 12:07:58 -0300

    Q5

--- Pratica 2/Q5/Q5.cpp
+++ Pratica 2/Q5/Q5.cpp
@@ -6,20 +6,20 @@ using namespace std;
 
 int main()
 {
-    float nota1, peso1;
-    float nota2, peso2;
-    float nota3, peso3;
+    int nota1, peso1;
+    int nota2, peso2;
+    int nota3, peso3;
     float notafinal;
 
     ifstream arquivo("notas.txt");
     arquivo >> nota1 >> peso1;
     arquivo >> nota2 >> peso2;
-    arquivo >> nota3 >> peso2;
+    arquivo >> nota3 >> peso3;
 
-    notafinal = ((nota1 * peso1) + (nota2 * peso2) + (nota3 * peso3)) / (peso1 + peso2 + peso3);
+    notafinal = ((nota1 * peso1) + (nota2 * peso2) + (nota3 * peso3)) / float(peso1 + peso2 + peso3);
 
     ofstream arquivo2("final.txt");
-    arquivo2 << fixed << setprecision(2) << notafinal;
+    arquivo2 << setprecision(2) << fixed << notafinal;
 
     return 0;
 }

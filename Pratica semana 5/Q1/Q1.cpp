9b2ef8e299f05c0990f444e533ee3004101def81
Whilker Henrique dos Santos Silva <77589533+Whilker-Silva@users.noreply.github.com>
2022-11-22 10:21:28 -0300

    Q1

--- Pratica semana 5/Q1/Q1.cpp
+++ Pratica semana 5/Q1/Q1.cpp
@@ -2,7 +2,26 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
 
-    
+    int numerador, denominador;
+    int quociente, resto;
+
+    cin >> numerador;
+    cin >> denominador;
+
+    if (denominador != 0)
+    {
+        quociente = numerador / denominador;
+        resto = numerador % denominador;
+
+        cout << quociente << endl
+             << resto;
+    }
+
+    else
+    {
+        cout << "erro";
+    }
 }

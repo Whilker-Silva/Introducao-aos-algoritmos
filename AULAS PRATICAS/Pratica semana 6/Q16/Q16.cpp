23620c529e36cc59c0dc1af8716f7e5a64e30be1
Whilker Henrique dos Santos Silva <77589533+Whilker-Silva@users.noreply.github.com>
2022-11-29 13:29:04 -0300

    Q16

--- AULAS PRATICAS/Pratica semana 6/Q16/Q16.cpp
+++ AULAS PRATICAS/Pratica semana 6/Q16/Q16.cpp
@@ -6,22 +6,21 @@ using namespace std;
 int main()
 {
     int tamanho;
-    int contador_linhas = 1;
-    int contador_colunas = 1;
+    int contador_linhas = 0;
+    int contador_colunas = 0;
 
     ofstream arq("jogo.txt");
 
     cin >> tamanho;
 
-    while (contador_linhas <= tamanho)
+    while (contador_linhas < tamanho)
     {
-        // em linhas impares
-        if ((contador_linhas % 2) != 0)
+        // em linhas Pares
+        if ((contador_linhas % 2) == 0)
         {
-            while (contador_colunas <= tamanho)
+            while (contador_colunas < tamanho)
             {
-                // em colunas impares
-                if ((contador_colunas % 2) != 0)
+                if ((contador_colunas % 2) == 0)
                 {
                     arq << ".";
                 }
@@ -30,17 +29,18 @@ int main()
                 {
                     arq << "#";
                 }
+
                 contador_colunas++;
-            }            
+            }
         }
 
-        // em linhas pares
+        // em linhas impares
         else
         {
-            while (contador_colunas <= tamanho)
+            while (contador_colunas < tamanho)
             {
                 // em colunas impares
-                if ((contador_colunas % 2) != 0)
+                if ((contador_colunas % 2) == 0)
                 {
                     arq << "#";
                 }
@@ -49,12 +49,13 @@ int main()
                 {
                     arq << ".";
                 }
+
                 contador_colunas++;
-            }            
+            }
         }
 
         arq << endl;
-        contador_colunas = 1;
+        contador_colunas = 0;
         contador_linhas++;
     }
     return 0;

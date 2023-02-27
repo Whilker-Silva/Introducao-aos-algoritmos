021c07c5e416f83a7515e70e0fd4a6c3de297723
Whilker Henrique dos Santos Silva <77589533+Whilker-Silva@users.noreply.github.com>
2023-02-27 19:48:15 -0300

    vetores Q10

--- AULAS PRATICAS/semana 8 (vetores)/Q10/Q10.cpp
+++ AULAS PRATICAS/semana 8 (vetores)/Q10/Q10.cpp
@@ -2,11 +2,117 @@
 
 using namespace std;
 
+enum meses
+{
+    janeiro,
+    fevereiro,
+    marco,
+    abril,
+    maio,
+    junho,
+    julho,
+    agosto,
+    setembro,
+    outubro,
+    novembro,
+    dezembro
+};
+
+void imprime(int mes)
+{
+    if (mes == janeiro)
+    {
+        cout << "janeiro" << endl;
+    }
+
+    else if (mes == fevereiro)
+    {
+        cout << "fevereiro" << endl;
+    }
+    if (mes == marco)
+    {
+        cout << "marco" << endl;
+    }
+
+    else if (mes == abril)
+    {
+        cout << "abril" << endl;
+    }
+    if (mes == maio)
+    {
+        cout << "maio" << endl;
+    }
+
+    else if (mes == junho)
+    {
+        cout << "junho" << endl;
+    }
+
+    else if (mes == junho)
+    {
+        cout << "junho" << endl;
+    }
+
+    else if (mes == julho)
+    {
+        cout << "julho" << endl;
+    }
+
+    else if (mes == agosto)
+    {
+        cout << "agosto" << endl;
+    }
+
+     else if (mes == setembro)
+    {
+        cout << "setembro" << endl;
+    }
+
+     else if (mes == outubro)
+    {
+        cout << "outubro" << endl;
+    }
+
+     else if (mes == novembro)
+    {
+        cout << "novembro" << endl;
+    }
+
+     else if (mes == dezembro)
+    {
+        cout << "dezembro" << endl;
+    }
+}
+
 int main()
 {
 
-    
-    
+    float temperaturas[12];
+    int posMaior = 0;
+    int posMenor = 0;
+
+    // preenche vetor
+    for (int i = 0; i < 12; i++)
+    {
+        cin >> temperaturas[i];
+    }
+
+    // buscar maior e menor
+    for (int i = 0; i < 12; i++)
+    {
+        if (temperaturas[i] > temperaturas[posMaior])
+        {
+            posMaior = i;
+        }
+
+        if (temperaturas[i] < temperaturas[posMenor])
+        {
+            posMenor = i;
+        }
+    }
+
+    imprime(posMaior);
+    imprime(posMenor);
+
     return 0;
 }
-

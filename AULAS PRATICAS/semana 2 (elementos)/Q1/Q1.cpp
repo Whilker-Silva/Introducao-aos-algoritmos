4a751c7d072c788ce97692c8dbe0dca6e0ea5eb0
Whilker Henrique dos Santos Silva <77589533+Whilker-Silva@users.noreply.github.com>
2023-08-21 15:03:19 -0300

    format

--- AULAS PRATICAS/semana 2 (elementos)/Q1/Q1.cpp
+++ AULAS PRATICAS/semana 2 (elementos)/Q1/Q1.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-	//decalrando variáveis
+	// decalrando variáveis
 	int numero = 0;
 	int quociente = 0;
 	int algarismo1 = 0;
@@ -12,34 +12,33 @@ int main()
 	int algarismo4 = 0;
 	int longitude = 0;
 	int latitude = 0;
-	
-	//atribuindo o valor da entrada na variavel numero
+
+	// atribuindo o valor da entrada na variavel numero
 	cin >> numero;
-	
-	//Calculo e atribuição do valor  do algarismo 4
-	algarismo4 = numero%10;
-	quociente = numero/10;
-	
-	//Calculo e atribuição do valor  do algarismo 4
-	algarismo3 = quociente%10;
-	quociente = quociente/10;
-	
-	//Calculo e atribuição do valor  do algarismo 4
-	algarismo2 = quociente%10;
-	quociente = quociente/10;
-	
-	//Calculo e atribuição do valor  do algarismo 4
-	algarismo1 = quociente%10;
-	quociente = quociente/10;
-	
-	//Calculo e atribuição do valor  do algarismo 4
-	latitude = algarismo1 + algarismo4; 
-	longitude = algarismo2 + algarismo3; 
-	
-	
-	//Mosrtando resultados
-	cout << latitude << endl;    
-	cout << longitude;	
-	
-    return 0;
+
+	// Calculo e atribuição do valor  do algarismo 4
+	algarismo4 = numero % 10;
+	quociente = numero / 10;
+
+	// Calculo e atribuição do valor  do algarismo 4
+	algarismo3 = quociente % 10;
+	quociente = quociente / 10;
+
+	// Calculo e atribuição do valor  do algarismo 4
+	algarismo2 = quociente % 10;
+	quociente = quociente / 10;
+
+	// Calculo e atribuição do valor  do algarismo 4
+	algarismo1 = quociente % 10;
+	quociente = quociente / 10;
+
+	// Calculo e atribuição do valor  do algarismo 4
+	latitude = algarismo1 + algarismo4;
+	longitude = algarismo2 + algarismo3;
+
+	// Mosrtando resultados
+	cout << latitude << endl;
+	cout << longitude;
+
+	return 0;
 }
